9de7b6e27ddc1b2950b1a78fcd95868597439d8e
Alek <92643775+barealek@users.noreply.github.com>
2024-12-03 08:27:13 +0100

    add doc string

--- pkg/mwm/mwmux.go
+++ pkg/mwm/mwmux.go
@@ -9,6 +9,12 @@ type mwm struct {
 	*http.ServeMux
 }
 
+// HandleFunc registers the handler function for the given pattern with the provided middleware functions.
+// The middleware functions are applied in the order they are provided.
+//
+// pattern: The URL pattern to match.
+// handler: The http.HandlerFunc to handle the request.
+// mws: A variadic list of middleware functions to apply to the handler. The first element of the list will be the first invoked middleware.
 func (m *mwm) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request), mws ...func(http.HandlerFunc) http.HandlerFunc) {
 	var h func(http.ResponseWriter, *http.Request) = handler
 
